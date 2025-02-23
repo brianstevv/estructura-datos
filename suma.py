4917dc660413fe21e6df2b00b978bec69cfa8203
brianstevv <brianstevenao@gmail.com>
2025-02-23 04:58:17 +0000

    arreglo

---
+++ suma.py
@@ -0,0 +1,8 @@
+def arreglo(arr, n):
+    if n == 0:
+        return 0
+    return arr[n - 1] + arreglo(arr, n - 1)
+
+numeros = [17, 29 , 9, 34 , 15]  
+resultado = arreglo(numeros, len(numeros))
+print(f"Suma de la lista: {resultado}")
